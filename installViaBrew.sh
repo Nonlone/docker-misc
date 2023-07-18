13a3c6de43238a764c3a42e591528c7009bba6c2
Nonlone <thunderbird.shun@gmail.com>
2023-07-18 14:12:30 +0800

    Update installViaBrew.sh
    调整脚本

--- installViaBrew.sh
+++ installViaBrew.sh
@@ -2,27 +2,29 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew update
-brew install oh-my-zsh
-brew install --cask google-chrome
-brew install onedrive
-brew install --cask keepassxc 
-brew install --cask iterm2
-brew install --cask raycast
-brew install --cask maczip
-brew install --cask itsycal
-brew install --cask maccy
-brew install --cask clashx
-brew install --cask mounty
-brew install --cask zoom
-brew install --cask matra
-brew install --cask visual-studio-code
-brew install --cask postman
-brew install --cask docker
-brew install --cask fork
-brew intalll --cask sequel-ace
-brew install zulu8
-brew install zulu11
-brew install zulu17
-brew install zulu
-brew install golang
+cd /usr/local/bin/
+
+./brew update
+./brew install zsh
+./brew install --cask google-chrome
+./brew install onedrive
+./brew install --cask keepassxc 
+./brew install --cask iterm2
+./brew install --cask raycast
+./brew install --cask maczip
+./brew install --cask itsycal
+./brew install --cask maccy
+./brew install --cask clashx
+./brew install --cask mounty
+./brew install --cask zoom
+./brew install --cask matra
+./brew install --cask visual-studio-code
+./brew install --cask postman
+./brew install --cask docker
+./brew install --cask fork
+./brew intalll --cask sequel-ace
+./brew install zulu8
+./brew install zulu11
+./brew install zulu17
+./brew install zulu
+./brew install golang
